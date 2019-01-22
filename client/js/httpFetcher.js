e4abef669f8325625340f8c31ff05be01c0ccfd0
Greg Kittleson <greg.kittleson@gmail.com>
2019-01-22 11:44:30 -0800

    Implement ajax call from client to server to move

--- client/js/httpFetcher.js
+++ client/js/httpFetcher.js
@@ -1,5 +1,16 @@
-(function() {
-
-  const serverUrl = '?';
-
-})();
+const fetcher = () => {
+  const serverUrl = 'http://127.0.0.1:3000';
+  $.ajax({
+    url: serverUrl,
+    type: 'GET',
+    contentType: 'application/json',
+    success: data => {
+      console.log(data);
+      SwimTeam.move(data);
+      console.log(`Sliiiiide to the ${data}. Take it back now y'all.`);
+    },
+    error: () => {
+      console.log('error: failed to retrieve command');
+    }
+  });
+};
--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -1,6 +1,10 @@
 const headers = require('./cors');
 
 module.exports = (req, res) => {
+  headers['Content-Type'] = 'application/json';
   res.writeHead(200, headers);
-  res.end();
+  var directions = ['up', 'down', 'left', 'right'];
+  var move = directions[Math.floor(Math.random() * 4)];
+  res.end(JSON.stringify(move));
+  return;
 };
